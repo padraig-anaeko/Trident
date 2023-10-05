ab1deaf872fc96178979b753fee7d3d99aa71914
bobjwalker <Bob.Walker@octopus.com>
2023-10-05 15:12:22 -0500

    Updating startup to support .net 7

--- src/Trident.Web/Program.cs
+++ src/Trident.Web/Program.cs
@@ -1,8 +1,14 @@
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
+using System;
+using System.Reflection;
 
 namespace Trident.Web
 {
@@ -10,18 +16,80 @@ namespace Trident.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfigureConfigurationFiles(builder);
+            ConfigureDependencyInjection(builder);
+            ConfigureLogging(builder);
+            ConfigureServices(builder);
+
+            builder.Host.UseNLog();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            }
+
+            app.UseWebOptimizer();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureLogging(logging =>
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddWebOptimizer(minifyJavaScript: false, minifyCss: false);
+            }
+            else
+            {
+                builder.Services.AddWebOptimizer();
+            }
+        }
+
+        private static void ConfigureConfigurationFiles(WebApplicationBuilder builder)
+        {
+            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            builder.Configuration.AddEnvironmentVariables();
+        }
+
+        private static void ConfigureDependencyInjection(WebApplicationBuilder builder)
+        {
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(autofacBuilder =>
                 {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Information);
-                })
-                .UseNLog()
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                    var assembly = Assembly.GetExecutingAssembly();
+
+                    autofacBuilder.RegisterAssemblyTypes(assembly)
+                        .Where(type => !type.IsAssignableTo<IHostedService>())
+                        .AsImplementedInterfaces();
+
+                    autofacBuilder.RegisterType<LoggerFactory>()
+                        .As<ILoggerFactory>()
+                        .InstancePerLifetimeScope();
+
+                    autofacBuilder.RegisterGeneric(typeof(Logger<>))
+                        .As(typeof(ILogger<>))
+                        .InstancePerLifetimeScope();
+                });
+        }
+
+        private static void ConfigureLogging(WebApplicationBuilder builder)
+        {
+            builder.Logging.ClearProviders();
+            builder.Logging.SetMinimumLevel(LogLevel.Information);
+        }
     }
 }
